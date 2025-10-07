123200d39b3395fa51d842a5f0c6291de8bd1268
Ethan Akiyama <ekakiyam@ucsc.edu>
2025-10-07 21:36:29 +0000

    step 2 complete: added button and counter and linked them up

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import "./style.css";
 
+let currentBears: number = 0;
+
 document.body.innerHTML = `
   <button id="bearButton">🐻</button>
-
+  <p>Bears: <span id="bearDisplay">0</span></p>
 `;
+
+const bearButton = document.getElementById("bearButton");
+const bearDisplay = document.getElementById("bearDisplay");
+
+if (bearButton && bearDisplay) {
+  bearButton.addEventListener("click", () => {
+    currentBears += 1;
+    bearDisplay.textContent = currentBears.toString();
+  });
+}
